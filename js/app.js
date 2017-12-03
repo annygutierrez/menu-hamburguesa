6892b7f5a2d639f549f100cd0679abbcbd826b18
Gloper98 <littleannygutlop_2014@outlook.es>
2017-12-03 17:47:14 -0500

    Updating files

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var button = document.getElementById("btn");
+
+function showMenu() {
+	var menu = document.getElementById("options-menu");
+	if(menu.classList.contains("disabled-menu")){
+		menu.classList.remove("disabled-menu");
+		menu.classList.add("enabled-menu");
+	} else {
+		menu.classList.remove("enabled-menu");
+		menu.classList.add("disabled-menu");
+	}
+}
+
+button.addEventListener("click", showMenu); 
+
+var navList = document.getElementsByClassName("nav-list");
+for(var i=0; i< navList.length; i++){
+	navList[i].addEventListener("click", showMenu);
+}
